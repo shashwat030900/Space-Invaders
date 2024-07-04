a678d778f9e2adc106300aeb17e54aa983d30190
shashwat030900 <3dartist.shashwat@gmail.com>
2024-07-04 22:35:02 +0530

    Update main.cpp

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,5 +1,31 @@
 
+#include <SFML/Graphics.hpp>
+
 int main()
 {
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = sf::VideoMode(800, 600);
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow window(videoMode, "SFML Window");
+
+
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        // Clear the window
+        window.clear(sf::Color::Blue);
+
+        // Display whatever you draw
+        window.display();
+    }
+
     return 0;
 }
