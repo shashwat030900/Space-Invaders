0d6acf74e5566d26c8411074454c2823f771d3f2
Shashwat <3dartist.shashwat@gmail.com>
2024-07-09 21:29:03 +0530

    Update main.cpp
    created shapes

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -8,6 +8,7 @@ int main()
 
     // Create a window object with specific dimensions and a title
     sf::RenderWindow window(videoMode, "SFML Window");
+   
 
 
 
@@ -21,12 +22,30 @@ int main()
 
 
         // Clear the window
-        window.clear(sf::Color::Blue);
+        window.clear(sf::Color::Black);
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Red);
+        circle.setPosition(400, 300); // Set position
+        window.draw(circle);
+        sf::CircleShape circle_2(20);
+        circle_2.setFillColor(sf::Color::Green);
+        window.draw(circle_2);
+        sf::RectangleShape Rectangle(sf::Vector2f(25,25));
+        Rectangle.setPosition(300, 400);
+        Rectangle.setFillColor(sf::Color::Red);
+        window.draw(Rectangle);
+        sf::ConvexShape Triangle;
+        Triangle.setPointCount(3);
+        Triangle.setPoint(0, sf::Vector2f(300, 50));
+        Triangle.setPoint(1, sf::Vector2f(200, 150));
+        Triangle.setPoint(2, sf::Vector2f(400, 150));
+        Triangle.setFillColor(sf::Color::Blue);
+        window.draw(Triangle);
 
         // Display whatever you draw
-        window.display(); //thanks for helping
+        window.display(); //thanks for helping 
 
     }
-
+    
     return 0;
 }
