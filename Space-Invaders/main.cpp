0065f930d73e7af4fc1ac38f02afdb11fcdd2419
Shashwat <3dartist.shashwat@gmail.com>
2024-07-11 23:05:34 +0530

    Update main.cpp

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,29 +1,32 @@
-
 #include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
 
-int main()
-{
-    // Define the video mode (dimensions)
+class Player {
+public:
+    sf::Texture player_texture;
+    sf::Sprite player_sprite;
+    int Health = 3;
+    sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
+    int movement_speed = 5;
+    int player_score = 0;
+};
+
+int main() {
+    Player myPlayer;
+    std::cout << myPlayer.movement_speed << "\n";
+
     sf::VideoMode videoMode = sf::VideoMode(800, 600);
-
-    // Create a window object with specific dimensions and a title
     sf::RenderWindow window(videoMode, "SFML Window");
-
-
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            // Check for window closure
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-
-        // Clear the window
-        window.clear(sf::Color::Blue);
-
-        // Display whatever you draw
+        window.clear(sf::Color::Green);
         window.display();
     }
 
