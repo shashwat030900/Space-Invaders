55bb3abadb32a4ddc8141035dbd7a80a7fed2c2d
Shashwat <3dartist.shashwat@gmail.com>
2024-07-15 17:42:29 +0530

    Update main.cpp
    added player ship

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 using namespace std;
+using namespace sf;
 
 class Player {
 private:
@@ -8,29 +9,44 @@ private:
     sf::Vector2f position = sf::Vector2f(200.0f, 100.0f);
     int movement_speed = 5;
     int player_score = 0;
+    
 public:  
     sf::Texture player_texture;
     sf::Sprite player_sprite;
     void takeDamage() {};
     void move() {};
     void shootBullets() {};
+    Vector2f getPosition() {
+        return position;
+    }
+      
 };
 
 int main() {
-    Player myPlayer;
-    std::cout << myPlayer.movement_speed << "\n";
+    
 
     sf::VideoMode videoMode = sf::VideoMode(800, 600);
     sf::RenderWindow window(videoMode, "SFML Window");
 
-    while (window.isOpen()) {
+    Player Player;
+    Player.player_texture.loadFromFile("assets/textures/player_ship.png");
+    Player.player_sprite.setTexture(Player.player_texture);
+        while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            Player.move();
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            Player.move();
 
+        }
         window.clear(sf::Color::Green);
+        Player.player_sprite.setPosition(Player.getPosition());
+        window.draw(Player.player_sprite);
         window.display();
     }
 
