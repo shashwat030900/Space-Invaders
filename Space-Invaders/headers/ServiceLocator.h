0c416e703490df02ec3b7c6b8e791fb3e3775974
Shashwat <3dartist.shashwat@gmail.com>
2024-08-01 15:47:23 +0530

    Update ServiceLocator.h

--- Space-Invaders/headers/ServiceLocator.h
+++ Space-Invaders/headers/ServiceLocator.h
@@ -1,1 +1,24 @@
 #pragma once
+
+
+class ServiceLocator
+{
+private:
+    
+    ServiceLocator();
+
+    ~ServiceLocator();
+
+
+    void createServices(); 			
+    void clearAllServices(); 		
+
+public:
+  
+    static ServiceLocator* getInstance(); 
+    void initialize(); 		
+    void update(); 			
+    void render(); 			
+
+   
+};
